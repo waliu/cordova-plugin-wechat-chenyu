e6bcc9ac00256717042c80680675a2d4a28feccd
waliu <250187715@qq.com>
2022-04-14 11:06:54 +0800

    no message

--- src/android/Pay.java
+++ src/android/Pay.java
@@ -15,16 +15,20 @@ public class Pay {
     this.api = api;
   }
 
-  public void sendPaymentRequest(JSONObject json,String appId) throws RuntimeException, JSONException {
+  public void sendPaymentRequest(JSONObject json,String appId) throws RuntimeException {
     PayReq req = new PayReq();
-    req.appId = appId;  // appId
-    req.partnerId = json.getString("partnerId");
-    req.prepayId = json.getString("prepayId");
-    req.nonceStr = json.getString("nonceStr");
-    req.timeStamp = json.getString("timestamp");
-    req.packageValue = json.getString("packageValue");
-    req.sign = json.getString("sign");
-    req.extData = "app data"; // optional
+    try {
+      req.appId = appId;  // appId
+      req.partnerId = json.getString("partnerId");
+      req.prepayId = json.getString("prepayId");
+      req.nonceStr = json.getString("nonceStr");
+      req.timeStamp = json.getString("timestamp");
+      req.packageValue = json.getString("packageValue");
+      req.sign = json.getString("sign");
+      req.extData = "app data"; // optional
+    } catch (JSONException e) {
+      throw new RuntimeException(Constants.Error501);
+    }
     boolean sendRes = api.sendReq(req);
     if (!sendRes) {
       throw new RuntimeException(Constants.Error503);
